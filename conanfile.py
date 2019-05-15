e8c6af6784afbda0ee002e615d59deb2fb07e9a2
Jan Bartusek <jan.bartusek@artin.cz>
2019-05-15 15:57:21 +0200

    fixed subsource directory member

--- conanfile.py
+++ conanfile.py
@@ -5,18 +5,18 @@ class NngppConan(ConanFile):
     name = "nngpp"
     version = "0.0.1"
     sha = "3351f54e6e774505d8d8b88064d04eb98e0b1cda"
-    source_folder = "source"
+    source_subfolder = "source"
     license = "MIT"
     url = "https://git.artin.cz/jan.bartusek/conan-nngpp"
     description = "nngpp conan receipe with nng roboauto dependency"
     requires = "nng/1.1.1@roboauto/stable"
 
     def source(self):
-        self.run("git clone https://github.com/cwzx/nngpp.git " + self.source_folder)
-        self.run("cd %s && git checkout %s" % (self.source_folder, self.sha))
+        self.run("git clone https://github.com/cwzx/nngpp.git " + self.source_subfolder)
+        self.run("cd %s && git checkout %s" % (self.source_subfolder, self.sha))
 
     def package(self):
         self.copy("*.h")
 
     def package_info(self):
-        self.cpp_info.includedirs = [os.path.join(self.source_folder, "include")]
+        self.cpp_info.includedirs = [os.path.join(self.source_subfolder, "include")]
